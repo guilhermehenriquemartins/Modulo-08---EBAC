53ce5fbb07155c0b08c33ba9e86fec81bf66b30d
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-09-11 23:28:11 -0300

    Início Aula 05

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const form = document.getElementById('form-atividade')
-let linhas = ''
-const NomeAtividade = document.getElementById('nome-atividade')
-const NotaAtividade = document.getElementById('nota-atividade')
 const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando"/>'
 const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji decepcionado"/>'
 
+let linhas = ''
+
+
 form.addEventListener('submit', function(e){
   e.preventDefault();
+
+  adicionaLinha();
+  atualizaPagina();
+})
+
+
+function adicionaLinha() {
+  const NomeAtividade = document.getElementById('nome-atividade')
+  const NotaAtividade = document.getElementById('nota-atividade')
 
   let linha = `<tr>`
   linha += `<td>${NomeAtividade.value}</td>`
@@ -16,9 +25,12 @@ form.addEventListener('submit', function(e){
 
   linhas += linha
 
+  NomeAtividade.value = ''
+  NotaAtividade.value = ''
+}
+
+
+function atualizaPagina() {
   const corpoTabela = document.querySelector('tbody')
   corpoTabela.innerHTML = linhas
-
-  NomeAtividade = ''
-  NotaAtividade = ''
-})
+}
