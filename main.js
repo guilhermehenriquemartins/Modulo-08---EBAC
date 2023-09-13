9957c24b2c22c4e3d4c13d23c7cb2148b5b07eed
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-09-12 23:44:49 -0300

    Continuação Aula 05

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const form = document.getElementById('form-atividade')
-const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando"/>'
-const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji decepcionado"/>'
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando"/>';
+const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji decepcionado"/>';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
-let linhas = ''
+let linhas = '';
 
 
 form.addEventListener('submit', function(e){
@@ -10,12 +14,16 @@ form.addEventListener('submit', function(e){
 
   adicionaLinha();
   atualizaPagina();
+  atualizaMediaFinal();
 })
 
 
 function adicionaLinha() {
   const NomeAtividade = document.getElementById('nome-atividade')
   const NotaAtividade = document.getElementById('nota-atividade')
+
+  atividades.push(NomeAtividade.value)
+  notas.push(parseFloat(NotaAtividade.value))
 
   let linha = `<tr>`
   linha += `<td>${NomeAtividade.value}</td>`
@@ -33,4 +41,23 @@ function adicionaLinha() {
 function atualizaPagina() {
   const corpoTabela = document.querySelector('tbody')
   corpoTabela.innerHTML = linhas
+}
+
+
+function atualizaMediaFinal() {
+  const notaMedia = calculaMediaFinal()
+
+  document.getElementById('nota-atividade-final').innerHTML = notaMedia;
+  document.getElementById('resultado-atividade-final').innerHTML = notaMedia >= 7 ? spanAprovado : spanReprovado
+}
+
+
+function calculaMediaFinal() {
+  let somaDasNotas = 0
+
+  for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
+  }
+  
+  return somaDasNotas/notas.length
 }
